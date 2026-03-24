c80e304778d853fe87ec9de5b900ffef01701069
nmg xb <flixihisnotok@gmail.com>
2026-03-24 05:41:46 +0000

    Manual code change

--- src/api/backendFunctions.js
+++ src/api/backendFunctions.js
@@ -2,3 +2,4 @@ import { base44 } from './base44Client';
 
 export const searchLeads = base44.functions.searchLeads;
 export const generatePreview = base44.functions.generatePreview;
+export const sendOutreach = base44.functions.sendOutreach;
