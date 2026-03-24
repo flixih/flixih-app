2f65112f487e89788f895de9f7bbcfb1cecfa0f9
nmg xb <flixihisnotok@gmail.com>
2026-03-24 05:45:58 +0000

    Manual code change

--- src/api/backendFunctions.js
+++ src/api/backendFunctions.js
@@ -1,5 +1,5 @@
 import { base44 } from './base44Client';
 
-export const searchLeads = (params) => base44.functions.searchLeads(params);
-export const generatePreview = (params) => base44.functions.generatePreview(params);
-export const sendOutreach = (params) => base44.functions.sendOutreach(params);
+export const searchLeads = (params) => base44.functions.invoke('searchLeads', params);
+export const generatePreview = (params) => base44.functions.invoke('generatePreview', params);
+export const sendOutreach = (params) => base44.functions.invoke('sendOutreach', params);
